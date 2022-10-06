b3c865e54cc28c50d19c7f69c26e834e8c9488b9
sergryap <rs1180@mail.ru>
2022-10-06 22:43:20 +0500

    Model Images register in admin.ModelAdmin

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -3,9 +3,20 @@ from .models import Place, Image
 
 
 # Register your models here.
+
+class ImageInline(admin.TabularInline):
+    model = Image
+    ordering = ['id']
+    extra = 5
+
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
+    inlines = [ImageInline]
     list_display = ['title', 'description_short', 'coordinate_lng', 'coordinate_lat']
     list_editable = ['description_short']
 
 
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'image', 'place']
