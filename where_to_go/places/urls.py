b79f1912af99a34e33ec75ffd9fe677914b3f707
sergryap <rs1180@mail.ru>
2022-10-07 18:23:43 +0500

    Add url 'details_url', add view 'details_url'

--- where_to_go/places/urls.py
+++ where_to_go/places/urls.py
@@ -1,10 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import start
+from .views import start, details_url
 
 urlpatterns = [
-    path('', start, name='start')
+    path('', start, name='start'),
+    path('places/<int:pk>', details_url, name='details_url')
 ]
 
 if settings.DEBUG:
--- where_to_go/places/views.py
+++ where_to_go/places/views.py
@@ -1,36 +1,55 @@
 from django.forms import model_to_dict
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 import json
 
+from django.urls import reverse
+
 from .models import Place
+
+
+def details_url(request, pk):
+    instances = Place.objects.all().prefetch_related('images')
+    instance = instances.get(pk=pk)
+    details = {
+        "title": instance.title,
+        "imgs": [img['image'] for img in instance.images.values()],
+        "description_short": instance.description_short,
+        "description_long": instance.description_long,
+        "coordinates": {
+            "lng": instance.coordinate_lng,
+            "lat": instance.coordinate_lat
+        }
+    }
+    return JsonResponse(details, safe=False, json_dumps_params={'ensure_ascii': False})
 
 
 # Create your views here.
 def start(request):
     template = 'places/index.html'
     places = Place.objects.all().prefetch_related('images')
-    place_list = []
+    features = []
+
     for place in places:
-        place_dict = {
-            "title": place.title,
-            "img": [d['image'] for d in place.images.values()],
-            "description_short": place.description_short,
-            "description_long": place.description_long,
-            "coordinates": {
-                "lng": place.coordinate_lng,
-                "lat": place.coordinate_lat
+        feature = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    round(float(place.coordinate_lng), 2),
+                    round(float(place.coordinate_lng), 6)
+                ]
+            },
+            "properties": {
+                "title": place.title.split("«")[1][:-1],
+                "placeId": place.pk,
+                "detailsUrl": reverse(details_url, kwargs={'pk': place.pk})
             }
         }
-        place_json = json.dumps(place_dict)
+        features.append(feature)
 
-        context_dict = {
-            "place_json": place_json,
-            "placeId": None,
-            "title": None,
-            "coordinates": None
-        }
-        place_list.append(context_dict)
-    context = {"place_list": place_list}
+    context = {"features": features}
 
     return render(request, template, context=context)
+
+
