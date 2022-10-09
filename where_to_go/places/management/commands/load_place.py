a8f450832725efc4d9b2567bae33297e5c4d87a6
sergryap <rs1180@mail.ru>
2022-10-09 17:12:20 +0500

    Modified load_place command

--- where_to_go/places/management/commands/load_place.py
+++ where_to_go/places/management/commands/load_place.py
@@ -10,7 +10,6 @@ class Command(BaseCommand):
     # Команда для парсинг данных из JSON-файла и записи в базу данных:
     # $ python3 manage.py load_place http://адрес/файла.json
 
-
     def add_arguments(self, parser):
         parser.add_argument('url', type=str)
 
@@ -19,19 +18,23 @@ class Command(BaseCommand):
         response = requests.get(url)
         response.raise_for_status()
         reviews = response.json()
-        reviews['coordinate_lng'] = reviews['coordinates']['lng']
-        reviews['coordinate_lat'] = reviews['coordinates']['lat']
-        del reviews['coordinates']
+        coordinates = reviews.pop('coordinates')
+        reviews['coordinate_lng'] = coordinates['lng']
+        reviews['coordinate_lat'] = coordinates['lat']
         imgs = reviews.pop('imgs')
-        place, created = Place.objects.get_or_create(**reviews)
+        place, created = Place.objects.get_or_create(
+            title=reviews.pop('title'),
+            defaults={**reviews})
 
-        for img in imgs:
-            filename = path.join('place_images', path.split(img)[1])
-            filename_all = path.join(MEDIA_ROOT, filename)
+        if created:
+            for img in imgs:
+                filename = path.join('place_images', path.split(img)[1])
+                filename_all = path.join(MEDIA_ROOT, filename)
+                Image.objects.get_or_create(
+                    image=filename,
+                    defaults={'place': place})
 
-            Image.objects.get_or_create(image=filename, place=place)
-
-            response = requests.get(img)
-            response.raise_for_status()
-            with open(filename_all, 'wb') as file:
-                file.write(response.content)
+                response = requests.get(img)
+                response.raise_for_status()
+                with open(filename_all, 'wb') as file:
+                    file.write(response.content)
