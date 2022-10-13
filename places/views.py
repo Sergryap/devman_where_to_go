b9e479c74849a497120e57525b3efd0b0534d319
sergryap <rs1180@mail.ru>
2022-10-13 16:47:53 +0500

    Modified view.py and settings.py

--- places/views.py
+++ places/views.py
@@ -2,22 +2,21 @@ from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from .models import Place
-from os import path
 
 
 def details_url(request, pk):
-    instance = get_object_or_404(Place.objects.all().prefetch_related('images'), pk=pk)
+    place = get_object_or_404(Place, pk=pk)
     details = {
-        "title": instance.title,
-        "imgs": [img.image.url for img in instance.images.order_by('position')],
-        "description_short": instance.description_short,
-        "description_long": instance.description_long,
-        "coordinates": {
-            "lng": instance.coordinate_lng,
-            "lat": instance.coordinate_lat
+        'title': place.title,
+        'imgs': [img.image.url for img in place.images.all()],
+        'description_short': place.description_short,
+        'description_long': place.description_long,
+        'coordinates': {
+            'lng': place.coordinate_lng,
+            'lat': place.coordinate_lat
         }
     }
-    return JsonResponse(details, safe=False, json_dumps_params={'ensure_ascii': False})
+    return JsonResponse(details, json_dumps_params={'ensure_ascii': False})
 
 
 def start(request):
@@ -28,20 +27,20 @@ def start(request):
     for place in places:
 
         feature = {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [
                     place.coordinate_lng,
                     place.coordinate_lat,
                 ]
             },
-            "properties": {
-                "title": place.title,
-                "placeId": place.pk,
-                "detailsUrl": reverse(details_url, kwargs={'pk': place.pk})
+            'properties': {
+                'title': place.title,
+                'placeId': place.pk,
+                'detailsUrl': reverse(details_url, kwargs={'pk': place.pk})
             }
         }
         features.append(feature)
-    places_geojson = {"type": "FeatureCollection", "features": features}
-    return render(request, template, context={"places_geojson": places_geojson})
+    places_geojson = {'type': 'FeatureCollection', 'features': features}
+    return render(request, template, context={'places_geojson': places_geojson})
