aed8e040826f8af95fe5ede99a79285b9fef8fac
sergryap <rs1180@mail.ru>
2022-10-16 12:47:09 +0500

    Modified load_place.py

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -5,6 +5,10 @@ import requests
 from os import path
 from bs4 import BeautifulSoup
 
+from environs import Env
+env = Env()
+env.read_env()
+
 
 class Command(BaseCommand):
     """
@@ -12,13 +16,15 @@ class Command(BaseCommand):
     """
 
     def add_arguments(self, parser):
-        parser.add_argument('-a', '--all', action='store_true',
-                            help='Полная загрузка с https://github.com/devmanorg/where-to-go-places/tree/master/places'
-                            )
+        parser.add_argument('-a', '--all', type=str, help='Полная загрузка с указанного url')
+        parser.add_argument('-ad', '--all_default', action='store_true', help='Загрузка из url по умолчанию')
         parser.add_argument('-u', '--url', type=str, help='Загрузка с одного указанного url')
 
     @staticmethod
-    def __upload_url(link):
+    def upload_url(link):
+        """
+        Загрузка данных для позиции на карте из ссылки link
+        """
         response = requests.get(link)
         response.raise_for_status()
         reviews = response.json()
@@ -47,19 +53,32 @@ class Command(BaseCommand):
         else:
             print(f'Позиция с названием "{title}" уже имеется в базе данных')
 
-    def handle(self, *args, **options):
-        upload_all = options['all']
-        link = options['url']
+    @staticmethod
+    def get_head_url_json(link_all):
+        """
+        Получение общей части ссылки на url json с данными для позиции на карте
+        """
+        details = link_all.split("/")
+        del details[details.index('tree')]
+        return '/'.join(['https://raw.githubusercontent.com'] + details[3:])
 
-        if upload_all:
+    def handle(self, *args, **options):
+        link_all = options['all']
+        link_one = options['url']
+        # если аргумент --all_default указан то загрузка по умолчанию
+        link_all_default = env('URL_PLACES_DEFAULT') if options['all_default'] else None
+        link_all = link_all if link_all else link_all_default
+
+        if link_all:
             # загрузка всех мест разом
-            src = requests.get('https://github.com/devmanorg/where-to-go-places/tree/master/places')
+            head_url_json = self.get_head_url_json(link_all)
+            src = requests.get(link_all)
             src.raise_for_status()
             soup = BeautifulSoup(src.text, 'lxml')
             link_blocks = soup.find_all(class_='js-navigation-open Link--primary')
             for block in link_blocks:
-                url = f'https://raw.githubusercontent.com/devmanorg/where-to-go-places/master/places/{block.text}'
-                self.__upload_url(url)
+                url = f'{head_url_json}/{block.text}'
+                self.upload_url(url)
         else:
             # загрузка по одному
-            self.__upload_url(link)
+            self.upload_url(link_one)
