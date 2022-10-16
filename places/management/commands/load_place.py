be4f84062dcf440e9f56d1cdd7e63710e060735a
sergryap <rs1180@mail.ru>
2022-10-16 13:54:16 +0500

    Modified load_place.py

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -21,35 +21,45 @@ class Command(BaseCommand):
         parser.add_argument('-u', '--url', type=str, help='Загрузка с одного указанного url')
 
     @staticmethod
-    def upload_url(link):
+    def get_location_details(link):
         """
-        Загрузка данных для позиции на карте из ссылки link
+        Получение данных о загружаемой локации из ссылки link
         """
         response = requests.get(link)
         response.raise_for_status()
-        place_details = response.json()
-        coordinates = place_details.pop('coordinates')
-        place_details['lng'] = coordinates['lng']
-        place_details['lat'] = coordinates['lat']
-        imgs = place_details.pop('imgs')
-        title = place_details.pop('title')
-        place, created = Place.objects.get_or_create(
-            title=title,
-            defaults=place_details)
+        place = response.json()
+        coordinates = place.pop('coordinates')
+        place['lng'] = coordinates['lng']
+        place['lat'] = coordinates['lat']
+        images = place.pop('imgs')
+        title = place.pop('title')
+        return *Place.objects.get_or_create(title=title, defaults=place), title, images
+
+    @staticmethod
+    def upload_images_to_place(images, place):
+        """
+        Загрузка фотографий для объекта 'place' в БД и в папку 'media' из списка 'images'
+        """
+        for img in images:
+            print(f'Загружаю фото: "{img}"')
+            filename = path.join('place_images', path.split(img)[1])
+            filename_all = path.join(MEDIA_ROOT, filename)
+            Image.objects.create(image=filename, place=place)
+            response = requests.get(img)
+            response.raise_for_status()
+            with open(filename_all, 'wb') as file:
+                file.write(response.content)
+
+    def upload_url(self, link):
+        """
+        Загрузка данных для локации на карте из ссылки link
+        """
+        place, created, title, images = self.get_location_details(link)
 
         if created:
             print(f'Создаю метку с названием: "{title}"')
-            for img in imgs:
-                print(f'Загружаю фото: "{img}"')
-                filename = path.join('place_images', path.split(img)[1])
-                filename_all = path.join(MEDIA_ROOT, filename)
-                Image.objects.create(image=filename, defaults={'place': place})
-                response = requests.get(img)
-                response.raise_for_status()
-                with open(filename_all, 'wb') as file:
-                    file.write(response.content)
-            print('Загрузка прошла успешно')
-            print(f'Добавлена метка с названием "{title}"')
+            self.upload_images_to_place(images, place)
+            print('Загрузка прошла успешно', f'Добавлена метка с названием "{title}"', sep='\n')
         else:
             print(f'Позиция с названием "{title}" уже имеется в базе данных')
 
