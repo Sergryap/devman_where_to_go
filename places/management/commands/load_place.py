6b8c88add484d062ee70ca5a98b3394fda5d597c
sergryap <rs1180@mail.ru>
2022-10-18 19:19:50 +0500

    Modified load_place.py

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -2,7 +2,6 @@ import re
 import requests
 from django.core.files.base import ContentFile
 from django.utils.crypto import md5
-from where_to_go.settings import MEDIA_ROOT
 from django.core.management.base import BaseCommand
 from places.models import Place, Image
 from bs4 import BeautifulSoup
@@ -14,24 +13,21 @@ env.read_env()
 
 class Command(BaseCommand):
     """
-    Команда для парсинг данных и записи в базу данных
+    Команда для парсинга данных и записи в базу данных
     """
 
     @staticmethod
-    def get_location_details(link):
+    def location_processing(location: dict):
         """
-        Получение данных о загружаемой локации из ссылки link
+        Обработка данных о загружаемой локации
         """
-        response = requests.get(link)
-        response.raise_for_status()
-        place = response.json()
-        coordinates = place.pop('coordinates')
-        place['lng'] = coordinates['lng']
-        place['lat'] = coordinates['lat']
-        images = place.pop('imgs')
-        title = place.pop('title')
+        coordinates = location.pop('coordinates')
+        location['lng'] = coordinates['lng']
+        location['lat'] = coordinates['lat']
+        images = location.pop('imgs')
+        title = location.pop('title')
 
-        return *Place.objects.get_or_create(title=title, defaults=place), title, images
+        return title, images, location
 
     @staticmethod
     def load_image(place: Place, image_content: bytes, position: int):
@@ -41,40 +37,31 @@ class Command(BaseCommand):
         content_file = ContentFile(image_content, name=md5(image_content).hexdigest())
         Image.objects.create(place=place, image=content_file, position=position)
 
-    def upload_images_to_place(self, images: list, place: Place):
-        """
-        Загрузка фотографий для объекта 'place' в БД и в папку 'media/place_images' из списка 'images'
-        """
-        for position, img in enumerate(images, start=1):
-            print(f'Загружаю фото: "{img}"')
-            response = requests.get(img)
-            response.raise_for_status()
-            self.load_image(
-                place=place,
-                image_content=response.content,
-                position=position
-            )
-
-    def upload_url(self, link):
+    def upload_url(self, link: str):
         """
         Загрузка всех данных для одной локации из ссылки link
         """
-        place, created, title, images = self.get_location_details(link)
+        response = requests.get(link)
+        response.raise_for_status()
+        title, images, location = self.location_processing(response.json())
+        place, created = Place.objects.get_or_create(title=title, defaults=location)
 
         if created:
             print(f'Создаю метку с названием: "{title}"')
-            self.upload_images_to_place(images, place)
+            for position, img in enumerate(images, start=1):
+                print(f'Загружаю фото: "{img}"')
+                response = requests.get(img)
+                response.raise_for_status()
+                self.load_image(place, response.content, position)
             print('Загрузка прошла успешно', f'Добавлена метка с названием "{title}"', sep='\n')
         else:
             print(f'Позиция с названием "{title}" уже имеется в базе данных')
 
-    def upload_all(self, link_of_places):
+    def upload_all(self, all_places: str):
         """
-        Загрузка всех данных для всех локаций из общей ссылки link_of_places
+        Загрузка всех данных для всех локаций
         """
-        src = requests.get(link_of_places)
-        src.raise_for_status()
-        soup = BeautifulSoup(src.text, 'lxml')
+        soup = BeautifulSoup(all_places, 'lxml')
         link_blocks = soup.find_all('a', attrs={'href': re.compile(r'.+\.json')})
         for block in link_blocks:
             url = ''.join(['https://raw.githubusercontent.com', block['href'].replace('blob/', '')])
@@ -97,7 +84,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         link_all = options['all']
         link_one = options['url']
-        # если аргумент --all_default указан то загрузка по умолчанию
+        # если аргумент --all_default указан, то загрузка по умолчанию
         link_all_default = (
             'https://github.com/devmanorg/where-to-go-places/tree/master/places'
             if options['all_default'] else None
@@ -106,7 +93,9 @@ class Command(BaseCommand):
 
         if link_all_places:
             # загрузка всех мест разом
-            self.upload_all(link_all_places)
+            src = requests.get(link_all_places)
+            src.raise_for_status()
+            self.upload_all(src.text)
         else:
             # загрузка по одному
             self.upload_url(link_one)
